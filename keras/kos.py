450ad5d10cd2ce963f425717c69c60a34e6f6cef
owen-jun <owenjin1993@gmail.com>
2019-08-02 16:34:57 +0900

    0802

--- keras/kos.py
+++ keras/kos.py
@@ -75,44 +75,55 @@ stand = MinMaxScaler(feature_range=(0, 8))
 open_price_list = np.array(kospi["open_price"])
 day_high_list = np.array(kospi["day_high"])
 day_low_list = np.array(kospi["day_low"])
+day_end_list = np.array(kospi["day_end"])
 
 open_price_list = open_price_list.reshape((-1,1))
 day_high_list = day_high_list.reshape((-1,1))
 day_low_list = day_low_list.reshape((-1,1))
+day_end_list = day_end_list.reshape((-1,1))
 
 stand.fit(open_price_list)
 open_price_list = stand.transform(open_price_list)
 day_high_list = stand.transform(day_high_list)
 day_low_list = stand.transform(day_low_list)
+day_end_list = stand.transform(day_end_list)
 
-kospi["open_price"],kospi["day_high"],kospi["day_low"] = open_price_list, day_high_list, day_low_list
+kospi["open_price"],kospi["day_high"],kospi["day_low"], kospi["day_end"] = open_price_list, day_high_list, day_low_list, day_end_list
 
 
-
+kospi= kospi.drop(["day"], axis=1)
+kospi_label = kospi_label.drop(["day"], axis=1)
 label = np.array(kospi_label)
 
 label
 
-kospi= kospi.drop(["day_end","day"], axis=1)
 
 kospi.head()
 
-band = MinMaxScaler(feature_range=(0, 5))
+# band = MinMaxScaler(feature_range=(0, 5))
 
-price_band_list = np.array(kospi["price_band"])
-price_band_list = price_band_list.reshape((-1,1))
+# price_band_list = np.array(kospi["price_band"])
+# price_band_list = price_band_list.reshape((-1,1))
 
-band.fit(price_band_list)
-price_band_list = band.transform(price_band_list)
+# band.fit(price_band_list)
+# price_band_list = band.transform(price_band_list)
 
-kospi["price_band"] = price_band_list
+# kospi["price_band"] = price_band_list
+
+
+
+
 kospi = kospi.drop(["open_price"], axis=1)
 kospi_list = np.array(kospi)
 
 kospi_list.shape
 
+
+###예측에 쓰이는 기간
 size = 3
 
+
+##TRAIN 데이터와 LABEL데이터를 나누는 곳
 def split_7(seq, size):
     aaa=[]
     for i in range(len(seq)-size+1):
@@ -124,51 +135,85 @@ def split_7(seq, size):
 
 def split_label(seq, size):
     lab = []
-    lab = seq[:,-1]
-    lab = lab[size:]
+    lab = np.array(lab)
+    lab = seq[:]
+    lab = np.append(lab[size:-1], lab[size+1:], axis=1)
     return lab
 
 x_train=split_7(kospi_list, size)
 print("==================")
-x_train = x_train.reshape(x_train.shape[0],x_train.shape[1]*x_train.shape[2],1)
+print(x_train[0])
+# x_train = x_train.reshape(x_train.shape[0],x_train.shape[1]*x_train.shape[2],1)
+x_train = x_train.reshape(x_train.shape[0],-1,1)
 
+print(x_train[0])
 x_train.shape
-x_pre = x_train[-1]
-x_train = x_train[:-1]
+x_map = x_train[-90:]
+y_map = label[-90+size:]
+x_pre = x_train[-2:]
+x_train = x_train[:-2]
 print(x_pre)
 
 label.shape
 y_train = split_label(label, size)
-
+# print(y_train)
 # print(label[:10])
 # print(y_train[:10])
 # print(x_train[0])
 print(x_train.shape)
 print(y_train.shape)
-x_pre = x_pre.reshape((1,x_pre.shape[0],1))
 print(x_pre.shape)
+# x_pre = x_pre.reshape((-1,x_pre.shape[0],1))
+print(x_pre.shape)
+
 
 x_tr, x_t, y_tr, y_t = train_test_split(x_train, y_train, random_state=66, test_size = 0.3)
 x_t, x_val, y_t, y_val = train_test_split(x_t, y_t, random_state=66, test_size = 0.5)
 ###x_train shape (593, 7, 6, 1)
+
+##모델
+
 model = Sequential()
-model.add(LSTM(10, input_shape=(x_tr.shape[1],1),return_sequences=True))
+model.add(LSTM(5, input_shape=(x_tr.shape[1],1),return_sequences=False))
 # model.add(Dropout(0.3))
 
-model.add(Dense(50,activation="relu"))
+model.add(Dense(500,activation="relu"))
+model.add(Dense(300,activation="relu"))
+model.add(Dense(100,activation="relu"))
+model.add(Dense(200,activation="relu"))
 # model.add(Dropout(0.3))
-model.add(Dense(30,activation="relu"))
+model.add(Dense(10,activation="relu"))
 
-model.add(Flatten())
-model.add(Dense(1))
-
+# model.add(Flatten())
+model.add(Dense(2))
+import keras
 
 model.compile(loss = "mse",optimizer="adam", metrics=['accuracy'])
-model.fit(x_tr,y_tr,epochs=1500,batch_size=20, verbose=2, validation_data=(x_val, y_val))
+early = keras.callbacks.EarlyStopping(monitor='val_loss',mode="auto",patience=20)
+model.fit(x_tr,y_tr,epochs=500,batch_size=50, verbose=2, validation_data=(x_val, y_val))
+
+
+
+#모델검증
 loss, acc = model.evaluate(x_t, y_t)
 y_ = model.predict(x_t)
-print(y_, "ori: ",y_t)
+for i in range(len(y_)):
+    print(y_[i], "ori: ",y_t[i])
 print(loss, acc)
 y_pre = model.predict(x_pre)
 print(y_pre)
 
+
+
+y_pre_map = model.predict(x_map)
+# print(y_pre_map)
+plt.plot(y_map)
+plt.plot(y_pre_map[:,0])
+
+plt.title("model kospi")
+plt.ylabel("end_price")
+
+plt.xlabel("day")
+plt.legend(["real_end","pre_end"], loc="upper left")
+plt.show()
+plt.show()

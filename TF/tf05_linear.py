ea549c3d0e2cf29b11c15586453986f00b687c45
owen-jun <owenjin1993@gmail.com>
2019-08-16 13:17:32 +0900

    day0816

--- TF/tf05_linear.py
+++ TF/tf05_linear.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 tf.set_random_seed(777)
 import numpy as np
-x = np.array([1,2,3]) ##train_set
+x = np.array([[1,1],[2,2],[3,3]]) ##train_set
 y = np.array([1,2,3]) ##train_y_set
 
 
 
 x_train = tf.placeholder(tf.float32)
-w = tf.Variable(tf.random_normal([1]), name="weight")
+w = tf.Variable([tf.random_normal([1]),tf.random_normal([1])
+], name="weight")
 b = tf.Variable(tf.random_normal([1]), name="bias")
 
 # hypothesis = x_train * w + b
@@ -31,7 +32,7 @@ feed_dict={x_train:x}
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     #### model learning#################################################
-    for step in range(30001):
+    for step in range(1001):
         # _, cost_val, w_val, b_val = sess.run([train, cost, w, b], feed_dict=feed_dict)
         _, cost_val, w_val, b_val = sess.run([train, cost, w, b])
 

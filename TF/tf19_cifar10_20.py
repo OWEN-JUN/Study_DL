816adcd41d957a53f2a3a5d7ce027627dc36919f
OWEN-JUN <49712113+OWEN-JUN@users.noreply.github.com>
2019-08-23 15:25:12 +0900

    day0823

--- TF/tf19_cifar10_20.py
+++ TF/tf19_cifar10_20.py
@@ -43,33 +43,27 @@ learning_rate = 0.001
 batch_size = 100
 
 L1 =tf.layers.conv2d(X,32,[2,2],activation=tf.nn.relu,padding="SAME")
-L1 = tf.layers.max_pooling2d(L1,[2,2],[2,2])
-# L1 = tf.layers.dropout(L1,0.7)
+L1 = tf.layers.max_pooling2d(L1,[2,2],[1,1],padding="SAME")
+L1 =tf.layers.conv2d(L1,32,[2,2],activation=tf.nn.relu,padding="SAME")
+L1 = tf.layers.max_pooling2d(L1,[2,2],[1,1],padding="SAME")
+L1 = tf.layers.dropout(L1,0.3)
 
 
 L2 =tf.layers.conv2d(L1,64,[3,3],activation=tf.nn.relu,padding="SAME")
-L2 = tf.layers.max_pooling2d(L2,[2,2],[1,1])
-# L1 = tf.layers.dropout(L1,0.7)
+L2 = tf.layers.max_pooling2d(L2,[2,2],[1,1],padding="SAME")
+L2 =tf.layers.conv2d(L2,64,[3,3],activation=tf.nn.relu,padding="SAME")
+L2 = tf.layers.max_pooling2d(L2,[2,2],[2,2])
+L2 = tf.layers.dropout(L2,0.4)
 
 
 L3 =tf.layers.conv2d(L2,128,[4,4],activation=tf.nn.relu)
 L3 = tf.layers.max_pooling2d(L3,[2,2],[1,1])
-
-W4 = tf.Variable(tf.random_normal([4, 4, 128, 256], stddev=0.01))
-            # kernel_size: (3,3), channel: 1(흑백), output: 32
-print("W4: ", W4)   # shape=(3, 3, 1, 32)
-#   Conv    ->   (?, 28, 28, 32)
-#   Pool    ->   (?, 14, 14, 32)
-L4 = tf.nn.conv2d(L3, W4, strides=[1, 1, 1, 1], padding='SAME')  # stride: 몇 칸씩 움직일 것인가
-#                                       [    ] 가운데 값 두개만 주로 쓴다. 바깥 2개는 거의 고정
-# print("L1: ", L1)   # shape=(?, 28, 28, 32)
-L4 = tf.nn.relu(L4)
-L4 = tf.nn.max_pool(L4, ksize=[1, 2, 2, 1], # (2, 2)로 자른 것을 2칸씩 이동 -> 반으로 줄어든다
-                      strides=[1, 2, 2, 1], padding='SAME')
-# print("L1: ", L1)   # shape=(?, 14, 14, 32)
+L3 =tf.layers.conv2d(L3,128,[4,4],activation=tf.nn.relu)
+L3 = tf.layers.max_pooling2d(L3,[2,2],[1,1])
+L3 = tf.layers.dropout(L3,0.5)
 
 
-L5 =tf.layers.flatten(L4)
+L5 =tf.layers.flatten(L3)
 # L5 = tf.layers.dense(L5,30,activation=tf.nn.relu)
 # L3 = tf.layers.dropout(L3,0.7)
 
